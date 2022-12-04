c2286f5390fba33aa944e7f556f519db9cef5399
Andrew Allison <andrew.allison@utah.edu>
2022-12-03 20:27:00 -0700

    Andrew's scraping for deaths/population

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import For, Str
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -5,35 +6,67 @@ from urllib.request import urlopen
 # https://opendata.ecdc.europa.eu/covid19/casedistribution/json/
 
 # Link to a website that has needed data which would be more compatible with scraping
-
 #https://www.worldometers.info/coronavirus/country/japan/
 #https://www.worldometers.info/world-population/us-population/
 
 #function can accept the website worldometers.info and country as parameters
 #it then joins them to create a new link to hyperlink to specific page of that country.
-#need to scrape the death toll and total population from the page
 def scrape_country (name, website):
         
-    combined = website + "country/" + name
-    #populationwebsite = website + "world-population/" + name + "-population"
+    countrywebsite = website + "/coronavirus/country/" + name #creates website for worldmeters.info based on country name received
     try:
-        page = urlopen(combined)
+        countrypage = urlopen(countrywebsite)
+    except:
+        print("Error connecting to the URL") 
+
+    
+    populationwebsite = website + "/world-population/" + name + "-population" #creates population website based on country name received
+    try:
+        populationpage = urlopen(populationwebsite)
     except:
         print("Error connecting to the URL")
-
     
-    soup = BeautifulSoup(page, 'html.parser')
-   
-    
-    #having an issue trying to scrape the death toll from the site.
-    content = soup.findAll(('div', {"class": "news_body"}))
-    stringcontent = str(content)
+    #code uses Beautiful soup to search website to find data phrase that is associated with the most recent total death counts.
+    countrysoup = BeautifulSoup(countrypage, 'html.parser')
+    countrycontent = countrysoup.findAll(('li', {"class": "news_li"}))
+    stringcontent = str(countrycontent)
     x=stringcontent.find('new deaths<')
-    #need to create code to extract number. x.isnumeric() is an option 
-    print(stringcontent[x-3] + stringcontent[x-2])
+    
+    #code searches back from found phrase to identify the number of most recent total deaths.
+    i=2 #number starts 2 characters before 'new deaths<'
+    deathlist = []
+    for i in range(11): #accounts for a maximum number of less than 10 billion
+        tempstr = stringcontent[x-i]
+        if tempstr.isnumeric() is True:
+            deathlist.append(stringcontent[x-i])
+        i+=1
+
+    strdeaths= "".join(deathlist[::-1])
+    intdeaths= int(strdeaths)
+    print("death count: " + strdeaths)
+    
+    #code uses Beautiful soup to search website to find data phrase that is associated with the most recent population of country.
+    populationsoup = BeautifulSoup(populationpage, 'html.parser')
+    populationcontent = populationsoup.findAll(('div', {"class": "content-inner"}))
+    stringcontent = str(populationcontent)
+    x=stringcontent.find('as of')
+    
+    #code searches back from found phrase to identify the country population
+    i=12 #number starts 12 characters before 'as of'
+    population = []
+    for i in range(22): #accounts for a maximum country population of less than 10 billion people
+        tempstr = stringcontent[x-i]
+        if tempstr.isnumeric() is True:
+            population.append(stringcontent[x-i])
+        i+=1
+
+    strpopulation = "".join(population[::-1])
+    intpopulation=int(strpopulation)
+    print(intpopulation)
+    normdeaths= intdeaths/(intpopulation/1000000) #normalize data received to population per 1 million people
+    print("normalized by population: " + str(normdeaths))
 
     with open('readme.txt', 'w') as f:
-        f.writelines(str(content))
-    #content.find('<strong>').get_text()
-
-scrape_country ("us", "https://www.worldometers.info/coronavirus/")
+        f.writelines(str(populationcontent))
+    
+scrape_country ("japan", "https://www.worldometers.info")
