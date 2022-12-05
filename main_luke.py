3802d45cdecfce82a1e2712c75da5b9c31b8c426
Andrew Allison <andrew.allison@utah.edu>
2022-12-05 06:10:17 -0700

    creates JSON data for 2nd day

--- main_luke.py
+++ main_luke.py
@@ -10,17 +10,17 @@ import ScrapeWebsite as SW
 
 website = 1
 country = ['japan', 'us', 'germany', 'china']
-filename = 'test1.json'
+filename = 'test1 2nd day.json'
 
 # run test case for website 1
 SW.scrape_country(website, filename, country)
 
 website = 2
-filename = 'test2.json'
+filename = 'test2 2nd day.json'
 # run test case for website 2
 SW.scrape_country(website, filename, country)
 
 website = 3
-filename = 'test3.json'
+filename = 'test3 2nd day.json'
 #creates data file for website 3
 SW.scrape_country(website, filename, country)
